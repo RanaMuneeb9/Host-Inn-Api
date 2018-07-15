9752ce5c59d3fbca0e71cc78923c6f10598cd243
Rana Muneeb <rana_mubi@live.com>
2018-07-16 01:00:04 +0500

    Minor Change
    Minor Change

--- Server.js
+++ Server.js
@@ -43,8 +43,8 @@ REST.prototype.configureExpress = function(connection) {
 }
 
 REST.prototype.startServer = function() {
-      app.listen(3300,function(){
-          console.log("All right ! I am alive at Port 3300.");
+      app.listen(3000,function(){
+          console.log("All right ! I am alive at Port 3000.");
       });
 }
 
